eaf465a6b5b0d01aaf4b074567fafe751922a50f
jhheng <jhheng1@gmail.com>
2016-08-19 16:04:43 +0800

    Update script.js

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -96,7 +96,23 @@ function buildAndShowHomeHTML (categories) {
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
-    function (homeHtml) {
+    function (homeHtmlUrl) {
+      $ajaxUtils.sendGetRequest{
+        categoryHtml, 
+        function (categories) {
+          // Switch CSS class active to menu button
+          switchMenuToActive();
+
+          var chosenCategoryShortName =
+            chooseRandomCategory(categories).short_name;
+          var homeHtmlToInsertIntoMainPage = 
+            insertProperty (categoryHtml,"randomCategoryShortName", chosenCategoryShortName.short_name);
+          insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+        },
+        false);
+    },
+    false);
+}
 
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
